03724efdddf1df2003881c8cd70e23432ced1f08
Vivek2cold <vk3699010@gmail.com>
2026-05-15 09:13:35 +0530

    Update cpu_program.java

--- cpu_program.java
+++ cpu_program.java
@@ -1,20 +1,14 @@
-// Java program to demonstrate inner class and static nested class
-// with user input
-
 import java.util.Scanner;
 
 class CPU {
 
     int price;
 
-    // Constructor
     CPU(int price) {
         this.price = price;
     }
 
-    // Inner class
     class Processor {
-
         int cores;
         String manufacturer;
 
@@ -22,17 +16,9 @@ class CPU {
             this.cores = cores;
             this.manufacturer = manufacturer;
         }
-
-        void displayProcessor() {
-            System.out.println("\nProcessor Details");
-            System.out.println("Number of Cores: " + cores);
-            System.out.println("Manufacturer: " + manufacturer);
-        }
     }
 
-    // Static nested class
     static class RAM {
-
         int memory;
         String manufacturer;
 
@@ -40,60 +26,46 @@ class CPU {
             this.memory = memory;
             this.manufacturer = manufacturer;
         }
-
-        void displayRAM() {
-            System.out.println("\nRAM Details");
-            System.out.println("Memory: " + memory + " GB");
-            System.out.println("Manufacturer: " + manufacturer);
-        }
     }
 }
 
-public class cpu_program {
+public class Main {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        // CPU input
         System.out.print("Enter CPU Price: ");
-        int price = sc.nextInt();
+        CPU cpu = new CPU(sc.nextInt());
         sc.nextLine();
 
-        // Processor input
         System.out.print("Enter Number of Processor Cores: ");
         int cores = sc.nextInt();
         sc.nextLine();
 
         System.out.print("Enter Processor Manufacturer: ");
-        String processorManufacturer = sc.nextLine();
+        String pMan = sc.nextLine();
 
-        // RAM input
         System.out.print("Enter RAM Memory (GB): ");
         int memory = sc.nextInt();
         sc.nextLine();
 
         System.out.print("Enter RAM Manufacturer: ");
-        String ramManufacturer = sc.nextLine();
+        String rMan = sc.nextLine();
 
-        // Create CPU object
-        CPU cpu = new CPU(price);
+        CPU.Processor p = cpu.new Processor(cores, pMan);
+        CPU.RAM r = new CPU.RAM(memory, rMan);
 
-        // Create Processor object
-        CPU.Processor processor =
-                cpu.new Processor(cores, processorManufacturer);
-
-        // Create RAM object
-        CPU.RAM ram =
-                new CPU.RAM(memory, ramManufacturer);
-
-        // Display details
         System.out.println("\n----- CPU DETAILS -----");
         System.out.println("CPU Price: " + cpu.price);
 
-        processor.displayProcessor();
+        System.out.println("\nProcessor Details");
+        System.out.println("Number of Cores: " + p.cores);
+        System.out.println("Manufacturer: " + p.manufacturer);
 
-        ram.displayRAM();
+        System.out.println("\nRAM Details");
+        System.out.println("Memory: " + r.memory + " GB");
+        System.out.println("Manufacturer: " + r.manufacturer);
 
         sc.close();
     }
