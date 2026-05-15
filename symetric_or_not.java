c6817744e3ffd89891ab1aa0dea02bfdb2ec7e2c
Vivek2cold <vk3699010@gmail.com>
2026-05-16 02:19:42 +0530

    Update symetric_or_not.java

--- symetric_or_not.java
+++ symetric_or_not.java
@@ -1,56 +1,32 @@
 import java.util.Scanner;
 
-class symatrix
-{
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter number of rows: ");
-        int rows = sc.nextInt();
-
-        System.out.print("Enter number of columns: ");
-        int cols = sc.nextInt();
-
-        int matrix[][] = new int[rows][cols];
-
-        if (rows != cols)
-        {
-            System.out.println("Matrix is NOT symmetric (not a square matrix).");
-            return;
-        }
-
-        System.out.println("Enter matrix elements:");
-
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                matrix[i][j] = sc.nextInt();
-            }
-        }
-
-        boolean symmetric = true;
-
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                if (matrix[i][j] != matrix[j][i])
-                {
-                    symmetric = false;
-                    break;
-                }
-            }
-        }
-
-        if (symmetric)
-        {
-            System.out.println("Matrix is SYMMETRIC");
-        }
-        else
-        {
-            System.out.println("Matrix is NOT SYMMETRIC");
-        }
-    }
-}
+ class symmatrix {
+     public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("enter matrix row : ");
+    int r = sc.nextInt();
+    System.out.print("enter matrix columns : ");
+    int c = sc.nextInt();
+    int matrix[][] = new int[r][c];
+    if(r!=c) {
+        System.out.println("matrix is not symmatric!!");
+        return;}
+    System.out.println("");
+    System.out.println("!!---matrix is symmetric---!!");
+    System.out.println("enter matrix elements : ");
+    for (int i =0; i<r; i++){
+        for(int j=0; j<c; j++){
+            matrix [i][j] = sc.nextInt();
+        }}
+    boolean symmetric = true;
+    for(int i =0; i<r; i++) {
+        for(int j=0; j<c; j++){
+            if(matrix[i][j] != matrix[j][i]){
+                symmetric = false;
+                break;
+            }}}
+    if(symmetric){
+        System.out.println("matrixis symmetric");
+    } else {
+        System.out.println("matrix is not symmetric!!");
+    }}}
